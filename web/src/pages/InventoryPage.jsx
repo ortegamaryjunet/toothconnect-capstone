6953e93883401f4ac667d398af7ac2745c195414
Maryjune Ortega <ortegamaryjunet@gmail.com>
2026-07-26 12:44:11 +0800

    Swap inventory logout modal actions

--- web/src/pages/InventoryPage.jsx
+++ web/src/pages/InventoryPage.jsx
@@ -2885,18 +2885,18 @@ export default function InventoryPage() {
             <div style={styles.modalActions}>
               <button
                 type="button"
-                style={{ ...styles.modalButton, ...styles.logoutBtn }}
-                onClick={handleLogout}
-              >
-                Logout
-              </button>
-
-              <button
-                type="button"
                 style={{ ...styles.modalButton, ...styles.cancelBtn }}
                 onClick={closeLogoutModal}
               >
                 Cancel
+              </button>
+
+              <button
+                type="button"
+                style={{ ...styles.modalButton, ...styles.logoutBtn }}
+                onClick={handleLogout}
+              >
+                Logout
               </button>
             </div>
           </div>
