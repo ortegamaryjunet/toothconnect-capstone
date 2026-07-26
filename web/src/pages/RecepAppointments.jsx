6491ecb3dca684b25a929caafdbf08609444f09f
Maryjune Ortega <ortegamaryjunet@gmail.com>
2026-07-26 12:53:36 +0800

    Update appointment pagination labels

--- web/src/pages/RecepAppointments.jsx
+++ web/src/pages/RecepAppointments.jsx
@@ -2561,7 +2561,7 @@ function Pagination({ styles, page, totalPages, onPrev, onNext }) {
         disabled={page <= 1}
         onClick={onPrev}
       >
-        <i className="fi fi-rr-angle-left"></i>
+        Prev
       </button>
 
       <span style={styles.pageInfo}>
@@ -2579,7 +2579,7 @@ function Pagination({ styles, page, totalPages, onPrev, onNext }) {
         disabled={page >= totalPages || totalPages === 0}
         onClick={onNext}
       >
-        <i className="fi fi-rr-angle-right"></i>
+        Next
       </button>
     </div>
   );
